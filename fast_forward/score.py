491457c6bbddc29106d8e63c95ba10bc94c0ecc8
maxfidi <m.fidlin@posteo.de>
2026-03-16 10:05:45 +0100

    typo in weights from thilos comment

--- fast_forward/score.py
+++ fast_forward/score.py
@@ -128,7 +128,7 @@ def score_matrix(molname, block, universe, distribution_files, file_prefix: str,
                 weights = [hellinger_weight, 1-hellinger_weight]
 
             # calculate score and populate matrix
-            score = calc_score(reference_data.T[1], probs, weigths, interaction_type='distances')
+            score = calc_score(reference_data.T[1], probs, weights, interaction_type='distances')
             score_matrix[node1, node2] = float(score)
             score_matrix[node2, node1] = float(score)
             plot_data['distances'][group_name] = {"x": reference_data.T[0],
